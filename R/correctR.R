779a00fbb4dd6f0b91806e2516a64d782e69852a
Trent <trent.henderson1@outlook.com>
2022-12-16 10:37:15 +1100

    first functions

---
+++ R/correctR.R
@@ -0,0 +1,9 @@
+#'
+#' @docType package
+#' @name correctR
+#' @title Corrections For Correlated Test Statistics
+#'
+#' @description Corrections For Correlated Test Statistics
+#'
+#' @importFrom stats var pt
+NULL
---
+++ R/kfold_t_test.R
@@ -0,0 +1,43 @@
+#' Compute correlated t-statistic and p-value for k-fold cross-validated results
+#' @importFrom stats var pt
+#' @param x \code{numeric} vector of classification accuracy values for classifier A
+#' @param y \code{numeric} vector of classification accuracy values for classifier B
+#' @param n \code{integer} denoting total sample size
+#' @param k \code{integer} denoting number of folds used in k-fold
+#' @return object of class \code{data.frame}
+#' @references Nadeau, C., and Bengio, Y. Inference for the Generalization Error. Machine Learning 52, (2003).
+#' @author Trent Henderson
+#' @export
+#'
+
+kfold.t.test <- function(x, y, n = length(x), k){
+
+  # Arg checks
+
+  if(length(x) != length(y)){
+    stop("x and y are not the same length.")
+  }
+
+  if(!is.numeric(x) | !is.numeric(y)){
+    stop("x and y should be numeric vectors of the same length.")
+  }
+
+  if(!is.numeric(n) | !is.numeric(k)){
+    stop("n and k should be integer scalars.")
+  }
+
+  # Calculations
+
+  d <- x - y # Calculate differences
+  sigma_2_mod <- stats::var(d, na.rm = TRUE) * (1/n + n2/n1) # Calculate modified variance
+  statistic <- mean(d, na.rm = TRUE) / sqrt(sigma_2 * ((1/n + (1/k)) / (1 - 1/k))) # Calculate t-statistic
+
+  if(statistic < 0){
+    p.value <- stats::pt(statistic, n - 1) # p-value for left tail
+  } else{
+    p.value <- stats::pt(statistic, n - 1, lower.tail = FALSE) # p-value for right tail
+  }
+
+  tmp <- data.frame(statistic = statistic, p.value = p.value)
+  return(tmp)
+}
