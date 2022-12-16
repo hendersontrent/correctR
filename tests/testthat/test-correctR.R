75d16a0009c24f0b9eaa0e6cd0456f0de1c1924f
Trent <trent.henderson1@outlook.com>
2022-12-16 16:00:03 +1100

    everything works

--- tests/testthat/test-correctR.R
+++ tests/testthat/test-correctR.R
@@ -1,6 +1,6 @@
 
 rss <- resampled_ttest(x = x, y = y, n = 30, n1 = 80, n2 = 20) # Random subsampling
-kcv <- kfold_ttest(x = y, y = y, n = 100, k = 30) # k-fold cross-validation
+kcv <- kfold_ttest(x = x, y = y, n = 100, k = 30) # k-fold cross-validation
 rkcv <- repkfold_ttest(data = tmp, n1 = 80, n2 = 20, k = 2, r = 2) # Repeated k-fold cross-validation
 
 test_that("random subsampling correction works", {
